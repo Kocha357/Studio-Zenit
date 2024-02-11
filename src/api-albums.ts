9742e4cb60494dd04aab4c4e67baeb3936f00aee
Victory <d.kovacevic@victory.rs>
2024-02-11 12:44:48 +0100

    formated code and added sort function for albums api call

--- src/api-albums.ts
+++ src/api-albums.ts
@@ -1,35 +1,47 @@
-async function fetchAlbums(
-	apiUrl: string
-): Promise<{ albums: { headingText: string; images: { imageUrl: string; imageAlt: string; aspectRatio: string }[] }[] }> {
-	try {
-		const response = await fetch(apiUrl);
+async function fetchAlbums(apiUrl: string): Promise<{
+  albums: {
+    headingText: string;
+    images: { imageUrl: string; imageAlt: string; aspectRatio: string }[];
+  }[];
+}> {
+  try {
+    const response = await fetch(apiUrl);
 
-		if (!response.ok) {
-			throw new Error("Network response was not ok");
-		}
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
 
-		const imgData = await response.json();
+    const imgData = await response.json();
 
-		const allImages: any[] = imgData?.data || [];
-		const albums = allImages.map((item: any) => {
-			const headingText: string = item?.attributes.headingText || "";
-			const images = item?.attributes?.images?.data || [];
+    const allImages: any[] = imgData?.data || [];
+    const sortedAllImages = allImages?.sort((a, b) => {
+      return (
+        new Date(b?.attributes?.publishedAt).getTime() -
+        new Date(a?.attributes?.publishedAt).getTime()
+      );
+    });
+    const albums = sortedAllImages.map((item: any) => {
+      const headingText: string = item?.attributes.headingText || "";
+      const images = item?.attributes?.images?.data || [];
 
-			const imageInfoArray = images.map((image: any) => {
-				const imageUrl = image?.attributes?.url || "";
-				const imageAlt = image?.attributes?.alternativeText || "";
-				const aspectRatio = image?.attributes?.width < image?.attributes?.height ? "portrait" : "landscape";
+      const imageInfoArray = images.map((image: any) => {
+        const imageUrl = image?.attributes?.url || "";
+        const imageAlt = image?.attributes?.alternativeText || "";
+        const aspectRatio =
+          image?.attributes?.width < image?.attributes?.height
+            ? "portrait"
+            : "landscape";
 
-				return { imageUrl, imageAlt, aspectRatio };
-			});
+        return { imageUrl, imageAlt, aspectRatio };
+      });
 
-			return { headingText, images: imageInfoArray };
-		});
+      return { headingText, images: imageInfoArray };
+    });
 
-		return { albums };
-	} catch (error) {
-		console.error("Error fetching data:", error);
-		throw error;
-	}
+    return { albums };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 }
 export default fetchAlbums;
--- src/api.ts
+++ src/api.ts
@@ -1,33 +1,39 @@
 async function fetchData(
-	url: string,
-	index: number
-): Promise<{ imageInfoArray: { imageUrl: string; imageAlt: string; aspectRatio: string }[]; aboutText?: string }> {
-	try {
-		const response = await fetch(url);
+  url: string,
+  index: number
+): Promise<{
+  imageInfoArray: { imageUrl: string; imageAlt: string; aspectRatio: string }[];
+  aboutText?: string;
+}> {
+  try {
+    const response = await fetch(url);
 
-		if (!response.ok) {
-			throw new Error("Network response was not ok");
-		}
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
 
-		const imgData = await response.json();
-		const aboutText: string = imgData?.data[0]?.attributes.aboutText || "";
-		const allImages: any[] = imgData?.data[index]?.attributes?.image?.data || [];
-		const imageInfoArray = allImages.map((image: any) => {
-			const imageUrl = image?.attributes?.url || "";
-			const imageAlt = image?.attributes?.alternativeText || "";
+    const imgData = await response.json();
+    const aboutText: string = imgData?.data[0]?.attributes.aboutText || "";
+    const allImages: any[] =
+      imgData?.data[index]?.attributes?.image?.data || [];
+    const imageInfoArray = allImages.map((image: any) => {
+      const imageUrl = image?.attributes?.url || "";
+      const imageAlt = image?.attributes?.alternativeText || "";
 
-			// Calculate aspect ratio (assuming naturalWidth and naturalHeight are available)
-			const aspectRatio = image?.attributes?.width < image?.attributes?.height ? "portrait" : "landscape";
-			console.log(image);
-			return { imageUrl, imageAlt, aspectRatio };
-		});
+      // Calculate aspect ratio (assuming naturalWidth and naturalHeight are available)
+      const aspectRatio =
+        image?.attributes?.width < image?.attributes?.height
+          ? "portrait"
+          : "landscape";
+      console.log(image);
+      return { imageUrl, imageAlt, aspectRatio };
+    });
 
-		return { imageInfoArray, aboutText };
-	} catch (error) {
-		console.error("Error fetching data:", error);
-		throw error;
-	}
+    return { imageInfoArray, aboutText };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 }
 
 export default fetchData;
-    
